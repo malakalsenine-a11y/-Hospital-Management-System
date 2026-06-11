fd38f9d4a91b73e134e88013fb988169aa6af6b1
Malakaka <malakalsenine@gmail.com>
2026-06-11 09:57:24 +0400

    added AvailableSlot class Property with data type

--- solution01/project01/Model/AvailableSlot.cs
+++ solution01/project01/Model/AvailableSlot.cs
@@ -8,5 +8,15 @@ namespace project01.Model
 {
     public class AvailableSlot
     {
+
+        public int slotId { get; set; }
+
+        public int doctorId { get; set; }
+
+        public string slotDate { get; set; }
+
+        public string slotTime { get; set; }
+
+        public bool isBooked { get; set; }
     }
 }
