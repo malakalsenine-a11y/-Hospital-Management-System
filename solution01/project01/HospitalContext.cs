896198b563da404640afa92fb103adb32896353c
Malakaka <malakalsenine@gmail.com>
2026-06-11 10:03:34 +0400

    added in HospitalContext class system storage

--- solution01/project01/HospitalContext.cs
+++ solution01/project01/HospitalContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using project01.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,15 @@ namespace project01
 {
     public class HospitalContext
     {
+        public List <Patient> Patients { get; set; }
+        public List<Doctor> Doctors { get; set; }
+
+        public List<Appointment> Appointments { get; set; }
+
+        public List<MedicalRecord> MedicalRecords { get; set; }
+
+        public List<AvailableSlot> AvailableSlots { get; set; }
+
+
     }
 }
