8d147fd6f57952f7989cfca27e332996e6ce67c5
Malakaka <malakalsenine@gmail.com>
2026-06-11 10:44:34 +0400

    inside Program complite Straightforward CRUD for ( Patient Registration)

--- solution01/project01/HospitalContext.cs
+++ solution01/project01/HospitalContext.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace project01
 {
     public class HospitalContext
--- solution01/project01/Program.cs
+++ solution01/project01/Program.cs
@@ -4,6 +4,48 @@ namespace project01
 {
     public class Program
     {
+
+        public static void PatientRegistration(HospitalContext context)
+        {
+            //Patient information:
+
+            Console.WriteLine("Enter Patient Name: ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Enter Patient Age: ");
+            int userAge = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Patient Gender: ");
+            string userGender = Console.ReadLine();
+
+            Console.WriteLine("Enter Patient Phone: ");
+            string userPhone = Console.ReadLine();
+
+            Console.WriteLine("Enter Patient Email: ");
+            string userEmail = Console.ReadLine();
+
+            Console.WriteLine("Enter Patient Blood Type: ");
+            string userBloodType = Console.ReadLine();
+
+            int userId = (context.Patients.Count) + 1;
+
+
+            //add patient:
+            context.Patients.Add(new Patient
+            {
+                patientId = userId,
+                patientName = userName,
+                patientAge = userAge,
+                patientGender = userGender,
+                patientPhone = userPhone,
+                patientEmail = userEmail,
+                patientBloodType = userBloodType
+            });
+
+            Console.WriteLine("Patient Added Successfully with ID" +userId);
+
+        
+        }
         static void Main(string[] args)
         {
             //Data storge for the system
@@ -20,3 +62,7 @@ namespace project01
         }
     }
 }
+
+
+// public int patientId { get; set; }
+
