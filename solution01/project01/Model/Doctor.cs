a7c4bbe294cad29d30fe145567773a9aab10b34c
Malakaka <malakalsenine@gmail.com>
2026-06-11 09:50:11 +0400

    added Doctor class Property with data type

--- solution01/project01/Model/Doctor.cs
+++ solution01/project01/Model/Doctor.cs
@@ -8,5 +8,19 @@ namespace project01.Model
 {
     public class Doctor
     {
+        public int doctorId { get; set; }
+
+        public string doctorName { get; set; }
+
+        public string doctorSpecialization { get; set; }
+
+        public string doctorPhone { get; set; }
+
+        public string doctorEmail { get; set; }
+
+
+        public decimal consultationFee { get; set; }
+
+
     }
 }
