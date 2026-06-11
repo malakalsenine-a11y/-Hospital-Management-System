b53c87261ebcb14818fe56be5db7ffe2d16f1264
Malakaka <malakalsenine@gmail.com>
2026-06-11 10:11:44 +0400

    In Main() you create one instance of this context, initialise all five lists as

--- solution01/project01/Program.cs
+++ solution01/project01/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace project01
+﻿using project01.Model;
+
+namespace project01
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Data storge for the system
+            HospitalContext mainContext = new HospitalContext();
+            mainContext.Patients = new List<Patient>();
+            mainContext.Doctors = new List<Doctor>();
+            mainContext.Appointments = new List<Appointment>();
+            mainContext.MedicalRecords = new List<MedicalRecord>();
+            mainContext.AvailableSlots = new List<AvailableSlot>();
+
+
+
+
         }
     }
 }
