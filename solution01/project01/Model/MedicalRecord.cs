534cb1dd2a5c34898049cb6730c8dfb265ca51e8
Malakaka <malakalsenine@gmail.com>
2026-06-11 09:55:50 +0400

    added MedicalRecord class Property with data type

--- solution01/project01/Model/MedicalRecord.cs
+++ solution01/project01/Model/MedicalRecord.cs
@@ -8,5 +8,23 @@ namespace project01.Model
 {
     public class MedicalRecord
     {
+        public int recordId { get; set; }
+
+        public int patientId { get; set; }
+
+        public int doctorId { get; set; }
+
+        public int appointmentId { get; set; }
+
+        public string diagnosis { get; set; }
+
+        public string prescription { get; set; }
+
+        public string visitDate { get; set; }
+
+        public decimal visitFee { get; set; }
+
+
+
     }
 }
